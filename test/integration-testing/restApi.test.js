776b7f290fc7c9eb454a3351bac0cbca5a394762
Mohibul Alam <bsse1004@iit.du.ac.bd>
2022-08-05 07:21:49 +0600

    index.js file added

--- test/integration-testing/restApi.test.js
+++ test/integration-testing/restApi.test.js
@@ -1,20 +1,17 @@
 const routes = require("./restApi");
 
 describe(`Rest Api Testing `, () => {
-    describe(`Rest Api Testing`, () => {
-        test(`get Course`, async () => {
-            const res = await routes.getCourse();
-            const data = res.status;
-            expect(data).toBe(200);
-        });
-
-        test(`Delete Employees`, async () => {
-            const emp = await routes.selectEmployees();
-            const employees = emp.data.view;
-            const id = employees[employees.length - 1].id;
-
-            const res = await routes.deleteEmployees({ id });
-            const data = res.status;
-            expect(data).toBe(200);
-        });
+    test(`get Course`, async () => {
+        const res = await routes.getCourse();
+        const data = res.status;
+        console.log(res);
+        expect(data).toBe(200);
     });
+
+    test(`add Course`, async () => {
+        const res = await routes.addCourse();
+        const data = res.status;
+        console.log(res);
+        expect(data).toBe(200);
+    });
+});
